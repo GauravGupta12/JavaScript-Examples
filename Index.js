42df7a0296696960efe8eb00920e3cadfd6de00f
Gaurav  Krishn <gauravkrishn@lpc-depaulsecure-218-186.depaulsecure-student.depaul.edu>
2018-04-05 19:15:28 -0500

    resolved merge conflict

--- Index.js
+++ Index.js
@@ -1,5 +1,5 @@
-﻿<script type="text/javascript">
+<script type="text/javascript">
     $(document).ready(function () {
-        
+        // jslocal2 made this change, at the same line number, this is a merge conflict
     })
 </script>
